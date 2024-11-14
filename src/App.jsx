62ef62a672de2c637d87cfe516666f69dc019498
omrs43 <omshirdhankar2847@gmail.com>
2024-11-14 19:28:25 +0530

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
 import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
-  //whya flex? so that two elements can be side by side
-  return (
-    <div className="flex p-9 ">
-        <Sidebar/>
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/favorites" element = {<FavoritesPage/>}/>
-        </Routes>
-    </div>
-  )
+	return (
+		<div className='flex'>
+			<Sidebar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/favorites' element={<FavoritesPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -2,28 +2,26 @@ import RecipeCard from "../components/RecipeCard";
 import { getRandomColor } from "../lib/utils";
 
 const FavoritesPage = () => {
-  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  return (
-    <div className="bg-[#faf9fb] flex-1 p-10 min0h-screen">
-      <div className="max-w-screen-lg mx-auto">
-        <p className="text-black font-bold tet-3xl md:text-5xl my-4">My Favorites</p>
+	const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-        {favorites.length === 0 && (
-            <div className="h-[80vh] flex flex-col items-center ga-4">
-              <img src="./404.svg" className="h-3/4" alt="404 svg" />
-            </div>
-        )}
+	return (
+		<div className='bg-[#faf9fb] flex-1 p-10 min-h-screen'>
+			<div className='max-w-screen-lg mx-auto'>
+				<p className='font-bold text-3xl md:text-5xl my-4'>My Favorites</p>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {favorites.map((recipe) => (
-              <RecipeCard key={recipe.label} recipe = {recipe} {...getRandomColor()}/>
-            ))}
+				{favorites.length === 0 && (
+					<div className='h-[80vh] flex flex-col items-center gap-4'>
+						<img src='/404.svg' className='h-3/4' alt='404 svg' />
+					</div>
+				)}
 
-          </div>
-      </div>
-
-    </div>
-  )
-}
-
+				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+					{favorites.map((recipe) => (
+						<RecipeCard key={recipe.label} recipe={recipe} {...getRandomColor()} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
 export default FavoritesPage;
